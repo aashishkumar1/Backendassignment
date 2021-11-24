a0674f2f627f48de063c9f5d3556e76a00a5b611
Aashish Kumar <91531431+aashishkumar1@users.noreply.github.com>
2021-11-24 14:28:25 +0530

    update

---
+++ middleware/app.js
@@ -0,0 +1,42 @@
+const user = require('./data.json');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send(user);
+});
+app.post('/user',(req,res)=>{
+    user.push(req.body);
+    res.send(user);
+});
+app.get('/user/:id',(req,res) => {
+   var arr =  user.filter((e)=>{
+        return e.id == req.params.id;
+    });
+    res.send(arr);
+});
+app.patch('/user/:id',(req,res) => {
+    var arr =  user.filter((e)=>{
+         return e.id == req.params.id;
+     });
+
+     var newData = req.body;
+
+     arr[0] = {
+         ...arr[0],
+         ...newData,
+     }
+     res.send(arr[0]);
+ });
+ app.delete('/user/:id',(req,res) =>{
+    var arr =  user.filter((e)=>{
+        return e.id != req.params.id;
+    });
+    res.send(arr);
+ });
+
+app.listen(2333,()=>{
+    console.log("Start")
+})
